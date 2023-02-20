340d132520a9bd82b222cbe2e819c8a4b9366ecb
Wassim Zahr <zahr@atlas17.ika.rwth-aachen.de>
2023-02-20 23:06:49 +0100

    Added mm3d framework and detr3d, gitignore modified

---
+++ detr3d/detr3d/models/utils/__init__.py
@@ -0,0 +1,5 @@
+from .detr import Deformable3DDetrTransformerDecoder
+from .detr3d_transformer import Detr3DTransformer, Detr3DTransformerDecoder, Detr3DCrossAtten
+
+__all__ = ['Deformable3DDetrTransformerDecoder', 
+           'Detr3DTransformer', 'Detr3DTransformerDecoder', 'Detr3DCrossAtten']
