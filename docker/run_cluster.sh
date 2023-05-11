d32ad1516beb17e05533b2660ec1bb89822c08ba
Wassim Zahr <zahr@rhea3.ika.rwth-aachen.de>
2023-05-11 20:01:43 +0200

    Updated gitignore

--- docker/run_cluster.sh
+++ docker/run_cluster.sh
@@ -2,12 +2,9 @@
 
 DOCKER_NAME=spatial_detr_ma_zahr
 
-
 # path to directory where nusenes data is stored
-nusc_data_dir="/work/data01/zahr/datasets/nuscenes"
-# nuscenes trainval
-nusc_train_val_data_dir="/work/data01/beemelmanns/nuscenes"
-# path to directory where models / logs shall be stored in
+nusc_data_dir="/work/data01/beemelmanns/nuscenes"
+# path to directory where model weights are stored
 work_dirs="/work/data01/zahr/work_dirs"
 # path to this repository root
 repo_dir=$PWD
@@ -26,8 +23,7 @@ docker run \
 --volume /tmp/.X11-unix:/tmp/.X11-unix:rw \
 --volume $HOME/.Xauthority:/root/.Xauthority:rw \
 --mount source=$repo_dir,target=/workspace,type=bind,consistency=cached \
---mount source=$nusc_data_dir,target=/workspace/data/nuscenes_mini,type=bind,consistency=cached \
---mount source=$nusc_train_val_data_dir,target=/workspace/data/nuscenes,type=bind,consistency=cached \
+--mount source=$nusc_data_dir,target=/workspace/data/nuscenes,type=bind,consistency=cached \
 --mount source=$work_dirs,target=/workspace/work_dirs,type=bind,consistency=cached \
 -it \
 $DOCKER_NAME
