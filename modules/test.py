7d3a33f6a6df00deca77a711f299c10b9da324d2
wassim <wzahr@I2200045.ika.rwth-aachen.de>
2023-10-02 10:42:19 +0200

    Scene selection and scene description info implemented

--- modules/test.py
+++ modules/test.py
@@ -1,6 +1,26 @@
-with open('scene_samples.txt', 'r') as file:
-    # Read lines from the file and convert them to integers
-    numbers = [int(line.strip()) for line in file]
+# Initialize empty lists to store the numbers and sentences
+numbers = []
+sentences = []
 
-# Now, the 'numbers' list contains the numbers from the file
-print(numbers)
+# Open the file for reading
+with open('scenes.txt', 'r') as file:
+    # Read each line in the file
+    for line in file:
+        # Split the line by the '|' character
+        parts = line.split('|')
+        
+        # Check if there are at least two parts (a number and a sentence)
+        if len(parts) >= 2:
+            # Extract and clean the number (assuming it's at the beginning of the first part)
+            number = int(parts[0].strip().split()[-1])
+            
+            # Extract and clean the sentence (after the '|' character)
+            sentence = parts[1].strip()
+            
+            # Append the number and sentence to their respective lists
+            numbers.append(number)
+            sentences.append(sentence)
+
+# Print the extracted numbers and sentences
+print("Numbers:", numbers)
+print("Sentences:", sentences)
