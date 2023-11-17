6300daafe0624c7f1aeb08bdff82ea55c2c0335a
Till Beemelmanns <till.beemelmanns@ika.rwth-aachen.de>
2023-11-17 16:26:59 +0100

    update auc calc script

--- scripts/auc.py
+++ scripts/auc.py
@@ -1,16 +1,29 @@
 import numpy as np
-import numpy as np
 import pandas as pd
+import os
 from scipy.integrate import trapz
 
-# Load data from text file
-data = pd.read_csv('/workspace/neg_pert/gradroll.txt', sep='\s+', names=['Step', 'NDS'], skiprows=1)
+directory = '/workspace/eval_results_final'
 
-# Convert Step values to integer and extract y values
-x_values = data['Step'].astype(int).values
-y_values = data['NDS'].values
+results = []
 
-# Calculate AUC using trapezoidal rule
-auc = trapz(y_values, x_values)
+# Iterate through all .dat files
+for filename in sorted(os.listdir(directory)):
+    if filename.endswith(".dat"):
+        data = pd.read_csv(os.path.join(directory, filename), sep='\s+', names=['perc', 'nds'], skiprows=1)
 
-print("The area under the curve is:", auc)
+        data = data[pd.to_numeric(data['perc'], errors='coerce').notnull()]
+        data['perc'] = data['perc'].astype(int)
+        data['nds'] = data['nds'].apply(pd.to_numeric, errors='coerce')
+
+        # Extract values and calculate AUC
+        x_values = data['perc'].values
+        y_values = data['nds'].values
+        auc = trapz(y_values, x_values)
+
+        results.append([filename, auc])
+
+# Write results to file
+with open('results.txt', 'w') as f:
+    for item in results:
+        f.write("%s %f\n" % (item[0], item[1]))
